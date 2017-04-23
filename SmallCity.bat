2bdf536ae184f457c24895d4aba7a2e2520d8356
Hazen Johnson <leyarotheconquerer@yahoo.com>
2017-04-23 17:26:33 -0500

    Refactor the startup script to be more system independent

--- SmallCity.bat
+++ SmallCity.bat
@@ -1,1 +1,1 @@
-E:\dev\urho3d\install\bin\Urho3DPlayer Data\LuaScripts\SmallCity.lua -w -pp E:\Users\Hazen\Projects\ld38
+%~dp0\Urho3DPlayer %~dp0Data\LuaScripts\SmallCity.lua -w -pp %~dp0
--- SmallCity.sh
+++ SmallCity.sh
@@ -1,1 +1,8 @@
-Urho3DPlayer Data/LuaScripts/SmallCity.lua -w -pp ./
+#!/usr/bin/bash
+SCRIPTPATH=`dirname $SCRIPT`
+if [ -e `which Urho3DPlayer` ]; then
+	Urho3DPlayer Data/LuaScripts/SmallCity.lua -w -pp $SCRIPTPATH
+else
+	echo "Please add Urho3DPlayer to your path"
+	echo "https://github.com/urho3d/urho3d"
+fi
