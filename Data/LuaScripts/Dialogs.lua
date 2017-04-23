252a81c2e88eaa90c1e7e02152935ddfe3ad4968
Hazen Johnson <leyarotheconquerer@yahoo.com>
2017-04-23 17:41:09 -0500

    Restrict dialogs to one instance per type

--- Data/LuaScripts/Dialogs.lua
+++ Data/LuaScripts/Dialogs.lua
@@ -8,19 +8,26 @@ dialogList = {
 	Tutorial3 = "UI/Tutorial4Dialog.xml",
 }
 dialogCallbacks = {}
+dialogCreated = {}
 
 function SpawnDialog(type, callback)
-	local dialog = ui.root:CreateChild("Window")
-	dialog:LoadXML(cache:GetResourceFileName(dialogList[type]))
-	dialog.style = "Window"
-	dialog.name = type
-	dialog:AddTag("Dialog")
+	if dialogCreated[type] == nil then
+		log:Write(LOG_DEBUG, "Creating dialog window "..type)
+		local dialog = ui.root:CreateChild("Window")
+		dialog:LoadXML(cache:GetResourceFileName(dialogList[type]))
+		dialog.style = "Window"
+		dialog.name = type
+		dialog:AddTag("Dialog")
 
-	dialogCallbacks[type] = callback
+		dialogCallbacks[type] = callback
+		dialogCreated[type] = true
 
-	local closers = dialog:GetChildrenWithTag("Close", true)
-	for i,closer in ipairs(closers) do
-		SubscribeToEvent(closer, "Pressed", "HandleCloseDialog")
+		local closers = dialog:GetChildrenWithTag("Close", true)
+		for i,closer in ipairs(closers) do
+			SubscribeToEvent(closer, "Pressed", "HandleCloseDialog")
+		end
+	else
+		log:Write(LOG_DEBUG, "Already have a dialog window of this type")
 	end
 end
 
@@ -36,5 +43,6 @@ function HandleCloseDialog(type, data)
 			dialogCallbacks[element.name] = nil
 		end
 		element:Remove()
+		dialogCreated[element.name] = nil
 	end
 end
